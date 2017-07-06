f52716ffd1c6820fa30a6f61bb4a1092df9a85a9
Davi Wesley <davi.wesley@icloud.com>
2017-07-06 16:46:51 -0300

    Update Teste.java

--- src/Teste.java
+++ src/Teste.java
@@ -18,23 +18,23 @@ public class Teste {
 		int opcao = -1;
 		do {
 			System.out.println("____________________________________");
-			System.out.println("   1 - Visualizar a árvore (com opções in-ordem, pré-ordem e pós-ordem).");
-			System.out.println("   2-  Buscar um elemento na árvore");
-			System.out.println("   3-  Remover um elemento na árvore.");
+			System.out.println("   1 - Visualizar a Ã¡rvore (com opÃ§Ãµes in-ordem, prÃ©-ordem e pÃ³s-ordem).");
+			System.out.println("   2-  Buscar um elemento na Ã¡rvore");
+			System.out.println("   3-  Remover um elemento na Ã¡rvore.");
 			System.out.println("   4-  Sair");
 			System.out.println("____________________________________");
-			System.out.println("Digite sua opção");
+			System.out.println("Digite sua opÃ§Ã£o");
 			opcao = input.nextInt();
 			switch (opcao) {
 			case 1:
 				int choice = -1;
 				do{
 					System.out.println("____________________________________");
-					System.out.println("   1 - Visualizar a árvore usando in-ordem");
-					System.out.println("   2-  Visualizar a árvore usando pré-ordem");
-					System.out.println("   3-  Visualizar a árvore usando pós-ordem).");
+					System.out.println("   1 - Visualizar a Ã¡rvore usando in-ordem");
+					System.out.println("   2-  Visualizar a Ã¡rvore usando prÃ©-ordem");
+					System.out.println("   3-  Visualizar a Ã¡rvore usando pÃ³s-ordem).");
 					System.out.println("____________________________________");
-					System.out.println("Digite sua opção");
+					System.out.println("Digite sua opÃ§Ã£o");
 					choice = input.nextInt();
 					switch(choice){
 					case 1:
@@ -51,34 +51,34 @@ public class Teste {
 						break;
 						
 					default:
-						System.out.println("Opção Inválida");
+						System.out.println("OpÃ§Ã£o InvÃ¡lida");
 					}
 				} while (choice != 0);
 				break;
 			case 2:
 				Scanner scanner = new Scanner(System.in);
-				System.out.println("Digite um número");
+				System.out.println("Digite um nÃºmero");
 				int escolha = scanner.nextInt();
 				if(b.find(escolha)){
 					System.out.println("Elemento encontrado");
 					b.display();
 					System.out.println();
 				}else{
-					System.out.println("Elemento não encontrado");
+					System.out.println("Elemento nÃ£o encontrado");
 					b.display();
 					System.out.println();
 				}
 				break;
 			case 3:
 				Scanner scanner1 = new Scanner(System.in);
-				System.out.println("Digite um número");
+				System.out.println("Digite um nÃºmero");
 				int escolha1 = scanner1.nextInt();
 				if(b.delete(escolha1)){
 					System.out.println("Elemento deletado");
 					b.display();
 					System.out.println();
 				}else{
-					System.out.println("Elemento não existe");
+					System.out.println("Elemento nÃ£o existe");
 					b.display();
 					System.out.println();
 				}
@@ -90,30 +90,6 @@ public class Teste {
 			}
 
 		} while (opcao != 0);
-	
-		/*b.insert(3);
-		b.insert(8);
-		b.insert(1);
-		b.insert(4);
-		b.insert(6);
-		b.insert(2);
-		b.insert(10);
-		b.insert(9);
-		b.insert(20);
-		b.insert(25);
-		b.insert(15);
-		b.insert(16);
-		System.out.println("Original Tree : ");
-		b.display();
-//		System.out.println("");
-//		System.out.println("Check whether Node with value 4 exists : " + b.find(4));
-//		System.out.println("Delete Node with no children (2) : " + b.delete(2));
-//		b.display();
-//		System.out.println("\n Delete Node with one child (4) : " + b.delete(4));
-//		b.display();
-//		System.out.println("\n Delete Node with Two children (10) : " + b.delete(10));
-//		b.display();
-*/
 	}
 
 }
